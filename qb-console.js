8d84366300e4faeaf9b70e2c0e6e1e9638422660
Parbin-kandel01 <107384234+Parbin-kandel01@users.noreply.github.com>
2025-09-27 12:02:02 +0545

    Update qb-console.js

--- qb-console.js
+++ qb-console.js
@@ -1,312 +1,135 @@
+// QB.js runtime for browser / WebView
 function _QB() {
-    // mininimal console-only implementation of the qb.js runtime library
-    // initially used only for the qbc.js command line compiler
-    var _rndSeed;
+    var _rndSeed = 123456;
 
     // Runtime Assertions
     function _assertParam(param, arg) {
-        if (arg == undefined) { arg = 1; }
-        if (param == undefined) { throw new Error("Method argument " + arg + " is required"); }
+        if (arg === undefined) { arg = 1; }
+        if (param === undefined) { throw new Error("Method argument " + arg + " is required"); }
     }
 
     function _assertNumber(param, arg) {
-        if (arg == undefined) { arg = 1; }
+        if (arg === undefined) { arg = 1; }
         if (isNaN(param)) { throw new Error("Number required for method argument " + arg); }
     }
 
-    // Array handling methods
-    // ----------------------------------------------------
+    // Array handling
     function initArray(dimensions, obj) {
         var a = {};
-        if (dimensions && dimensions.length > 0) {
-            a._dimensions = dimensions;
-        }
-        else {
-            // default to single dimension to support Dim myArray() syntax
-            // for convenient hashtable declaration
-            a._dimensions = [{l:0,u:1}];
-        }
+        a._dimensions = (dimensions && dimensions.length) ? dimensions : [{l:0,u:1}];
         a._newObj = { value: obj };
         return a;
-    };
+    }
 
-    function resizeArray (a, dimensions, obj, preserve) {
-       if (!preserve) {
-            var props = Object.getOwnPropertyNames(a);
-            for (var i = 0; i < props.length; i++) {
-                if (props[i] != "_newObj") {
-                    delete a[props[i]];
-                }
-            }
+    function resizeArray(a, dimensions, obj, preserve) {
+        if (!preserve) {
+            Object.keys(a).forEach(k => {
+                if (k !== "_newObj") delete a[k];
+            });
         }
-        if (dimensions && dimensions.length > 0) {
-            a._dimensions = dimensions;
-        }
-        else {
-            // default to single dimension to support Dim myArray() syntax
-            // for convenient hashtable declaration
-            a._dimensions = [{l:0,u:1}];
-        }
+        a._dimensions = (dimensions && dimensions.length) ? dimensions : [{l:0,u:1}];
     }
 
     function arrayValue(a, indexes) {
         var value = a;
         for (var i=0; i < indexes.length; i++) {
-            if (value[indexes[i]] == undefined) {
-                if (i == indexes.length-1) {
-                    value[indexes[i]] = JSON.parse(JSON.stringify(a._newObj));
-                }
-                else {
-                    value[indexes[i]] = {};
-                }
+            if (value[indexes[i]] === undefined) {
+                value[indexes[i]] = (i === indexes.length-1) ? JSON.parse(JSON.stringify(a._newObj)) : {};
             }
             value = value[indexes[i]];
         }
-
         return value;
     }
 
-    function halt() {
-        /*_haltedFlag = true;
-        _runningFlag = false;
-        _inputMode = false;
-        GX.soundStopAll();
-        toggleCursor(true);*/
-    }
+    // Halt and autoLimit (placeholders)
+    function halt() { console.warn("Halt called"); }
+    function halted() { return false; }
+    function autoLimit() { /* placeholder for yield in async loops */ }
 
-    function halted() {
-        return false;
-        //return _haltedFlag;
-    };
-
-    function autoLimit() {
-        /*var timeElapsed = new Date() - _lastLimitTime;
-        if (timeElapsed > 1000) { 
-            _flushAllScreenCache();
-            await GX.sleep(1);
-            _lastLimitTime = new Date();
-        }*/
-    }
-
+    // String functions
     function func_Asc(value, pos) {
         _assertParam(value);
-        if (pos == undefined) {
-            pos = 0;
-        }
-        else {
-            _assertNumber(pos, 2);
-            pos--; 
-        }
-
-        var c = String(value).charCodeAt(pos);
-        //var uc = _ccharMap[c];
-        //if (uc) { c = uc; }
-        return c;
+        pos = (pos === undefined) ? 0 : pos-1;
+        return String(value).charCodeAt(pos);
     }
 
     function func_Chr(charCode) {
         _assertNumber(charCode);
-        //var uc = _ucharMap[charCode];
-        //if (uc) { charCode = uc; }
         return String.fromCharCode(charCode);
     }
 
-    function func_Command() {
-        return "";
-    }
+    function func_Command() { return ""; }
 
-    async function sub_Fetch (url, fetchRes) {
-        const fs = require("fs");
-        const data = fs.readFileSync(url, 'utf8');
-        fetchRes.ok = true;
-        fetchRes.text = data;
+    async function sub_Fetch(url, fetchRes) {
+        try {
+            const response = await fetch(url);
+            fetchRes.ok = response.ok;
+            fetchRes.text = await response.text();
+        } catch (e) {
+            fetchRes.ok = false;
+            fetchRes.text = "";
+        }
     }
 
     async function func_Fetch(url) {
         var fetchRes = {};
-        await QB.sub_Fetch(url, fetchRes);
+        await sub_Fetch(url, fetchRes);
         return fetchRes;
     }
 
-    function func_Left(value, n) {
-        _assertParam(value, 1);
-        _assertNumber(n, 2);
-        return String(value).substring(0, n);
+    function func_Left(value, n) { _assertParam(value,1); _assertNumber(n,2); return String(value).substring(0,n); }
+    function func_Len(value) { _assertParam(value); return String(value).length; }
+    function func_LTrim(value) { _assertParam(value); return String(value).trimStart(); }
+    function func_Mid(value, n, len) { 
+        _assertParam(value,1); _assertNumber(n,2); 
+        return (len === undefined) ? String(value).substring(n-1) : String(value).substring(n-1,n+len-1); 
     }
-
-    function func_InStr(arg1, arg2, arg3) {
-        _assertParam(arg1, 1);
-        _assertParam(arg2, 2);
-        var startIndex = 0;
-        var strSource = "";
-        var strSearch = "";
-        if (arg3 != undefined) {
-            startIndex = arg1-1;
-            strSource = String(arg2);
-            strSearch = String(arg3);
-        }
-        else {
-            strSource = String(arg1);
-            strSearch = String(arg2);
-        }
-        return strSource.indexOf(strSearch, startIndex)+1;
-    }
-
-    function func__InStrRev(arg1, arg2, arg3) {
-        _assertParam(arg1, 1);
-        _assertParam(arg2, 2);
-        var startIndex = +Infinity;
-        var strSource = "";
-        var strSearch = "";
-        if (arg3 != undefined) {
-            startIndex = arg1-1;
-            strSource = String(arg2);
-            strSearch = String(arg3);
-        }
-        else {
-            strSource = String(arg1);
-            strSearch = String(arg2);
-        }
-        return strSource.lastIndexOf(strSearch, startIndex)+1;
-    }
-    
-    function func_LCase(value) {
-        _assertParam(value);
-        return String(value).toLowerCase();
-    };
-
-    function func_Left(value, n) {
-        _assertParam(value, 1);
-        _assertNumber(n, 2);
-        return String(value).substring(0, n);
-    }
-
-    function func_Len(value) {
-        _assertParam(value);
-        return String(value).length;
-    }
-
-    function func_LTrim(value) {
-        _assertParam(value);
-        return String(value).trimStart();
-    }
-
-    function func_Mid(value, n, len) {
-        _assertParam(value, 1);
-        _assertNumber(n, 2);
-        if (len == undefined) {
-            return String(value).substring(n-1);
-        }
-        else {
-            return String(value).substring(n-1, n+len-1);
-        }
-    }
- 
-    function func_Right(value, n) {
-        _assertParam(value, 1);
-        _assertNumber(n, 2);
-        if (value == undefined) {
-            return "";
-        }
-        var s = String(value);
-        return s.substring(s.length-n, s.length);
-    }
-
-    function func__Round(value) {
-        _assertNumber(value);
-        if (value < 0) {
-            return -Math.round(-value);
-        } else {
-            return Math.round(value);
-        }
-    }
-
+    function func_Right(value, n) { _assertParam(value,1); _assertNumber(n,2); return String(value).substring(String(value).length-n); }
+    function func__Round(value) { _assertNumber(value); return Math.round(value); }
     function func_Rnd(n) {
-        if (n == undefined) {n = 1;}
-        if (n != 0) {
-            if (n < 0) {
-                const buffer = new ArrayBuffer(8);
-                const view = new DataView(buffer);  
-                view.setFloat32(0, n, false);
-                var m = view.getUint32(0);
-                _rndSeed = (m & 0xFFFFFF) + ((m & 0xFF000000) >>> 24);
-            }
+        if (n === undefined) n=1;
+        if (n !== 0) {
+            if (n < 0) _rndSeed = Math.abs(n*1234567)%0xFFFFFF;
             _rndSeed = (_rndSeed * 16598013 + 12820163) & 0xFFFFFF;
         }
-        return _rndSeed / 0x1000000;
+        return _rndSeed/0x1000000;
     }
-
-    function func_RTrim(value) {
-        _assertParam(value);
-        return String(value).trimEnd();
-    }
-
-    function func_Str(value) {
-        return String(value);
-    }
-
-    function func_String(ccount, s) {
-        _assertNumber(ccount, 1);
-        _assertParam(s, 2);
-        if (typeof s === "string") {
-            s = s.substring(0, 1);
-        }
-        else {
-            s = String.fromCharCode(s);
-        }
+    function func_RTrim(value) { _assertParam(value); return String(value).trimEnd(); }
+    function func_Str(value) { return String(value); }
+    function func_String(ccount, s) { 
+        _assertNumber(ccount,1); _assertParam(s,2);
+        s = (typeof s==="string") ? s[0] : String.fromCharCode(s);
         return "".padStart(ccount, s);
     }
+    function func__Trim(value) { _assertParam(value); return String(value).trim(); }
+    function func_UBound(a, dimension) { _assertParam(a); dimension = (dimension===undefined)?1:dimension; _assertNumber(dimension,2); return a._dimensions[dimension-1].u; }
+    function func_UCase(value) { _assertParam(value); return String(value).toUpperCase(); }
+    function func_LCase(value) { _assertParam(value); return String(value).toLowerCase(); }
 
-    function func__Trim(value) {
-        _assertParam(value);
-        return value.trim();
+    function func_InStr(arg1,arg2,arg3){
+        _assertParam(arg1,1); _assertParam(arg2,2);
+        var startIndex=0, strSource="", strSearch="";
+        if(arg3!==undefined){ startIndex=arg1-1; strSource=String(arg2); strSearch=String(arg3); }
+        else { strSource=String(arg1); strSearch=String(arg2); }
+        return strSource.indexOf(strSearch,startIndex)+1;
     }
 
-    function func_UBound(a, dimension) {
-        _assertParam(a);
-        if (dimension == undefined) {
-            dimension = 1;
-        }
-        else {
-            _assertNumber(dimension, 2);
-        }
-        return a._dimensions[dimension-1].u;
-    }
-
-    function func_UCase(value) {
-        _assertParam(value);
-        return String(value).toUpperCase();
+    function func__InStrRev(arg1,arg2,arg3){
+        _assertParam(arg1,1); _assertParam(arg2,2);
+        var startIndex=+Infinity, strSource="", strSearch="";
+        if(arg3!==undefined){ startIndex=arg1-1; strSource=String(arg2); strSearch=String(arg3); }
+        else { strSource=String(arg1); strSearch=String(arg2); }
+        return strSource.lastIndexOf(strSearch,startIndex)+1;
     }
 
     return {
-        initArray: initArray,
-        resizeArray: resizeArray,
-        arrayValue: arrayValue,
-        autoLimit: autoLimit,
-        halt: halt,
-        halted: halted,
-        func_Asc: func_Asc,
-        func_Chr: func_Chr,
-        func_Command: func_Command,
-        func_Fetch: func_Fetch,
-        sub_Fetch: sub_Fetch,
-        func_InStr: func_InStr,
-        func__InStrRev: func__InStrRev,
-        func_LCase: func_LCase,
-        func_Left: func_Left,
-        func_Len: func_Len,
-        func_LTrim: func_LTrim,
-        func_Mid: func_Mid,
-        func_Right: func_Right,
-        func__Round: func__Round,
-        func_Rnd: func_Rnd,
-        func_RTrim: func_RTrim,
-        func_Str: func_Str,
-        func_String: func_String,
-        func__Trim: func__Trim,
-        func_UBound: func_UBound,
-        func_UCase: func_UCase
+        initArray, resizeArray, arrayValue, autoLimit, halt, halted,
+        func_Asc, func_Chr, func_Command, func_Fetch, sub_Fetch,
+        func_InStr, func__InStrRev, func_LCase, func_Left, func_Len,
+        func_LTrim, func_Mid, func_Right, func__Round, func_Rnd,
+        func_RTrim, func_Str, func_String, func__Trim, func_UBound, func_UCase
     };
 }
 
-module.exports.QB = _QB;
+// Expose QB globally for browser
+window.QB = _QB();
